18ddf0df0061ed05965c97a1df30eb234ed7ffdd
Beeja Divya <Beeja.Divya@ivycomptech.com>
2022-08-08 21:58:48 +0530

    1

---
+++ DayA1/src/Day1A1.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+public class Day1A1 {
+    static void printCollatz(int n)
+    {
+        while (n != 1)
+        {
+            System.out.print(n + " ");
+
+            if ((n & 1) == 1)
+                n = 3 * n + 1;
+            else
+                n = n / 2;
+        }
+
+        System.out.print(n);
+    }
+
+    public static void main (String[] args)
+    {
+        printCollatz(5);
+    }
+}
+
+
+
---
+++ DayA1/src/Day1A2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+    public class Day1A2 {
+
+
+        public static void main(String args[])
+
+        {
+
+            Scanner d=new Scanner(System.in);
+
+            int m=d.nextInt();
+
+            int n=d.nextInt();
+
+            int l=d.nextInt();
+
+            if(l==10||l==11||l==16||l==17||l==18||l==19||l==20||l==5||l==4||l==2||l==3)
+
+            {
+
+                System.out.println("Yes");
+
+            }
+
+            else
+
+            {
+
+                System.out.println("No");
+
+            }
+
+        }
+
+    }
