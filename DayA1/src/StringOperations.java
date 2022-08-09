cc90a1cc9a8399659656f34e0675b6069b83917d
Beeja Divya <Beeja.Divya@ivycomptech.com>
2022-08-09 15:43:24 +0530

    string

--- DayA1/src/StringOperations.java
+++ DayA1/src/StringOperations.java
@@ -1,23 +1,24 @@
 
+import java.util.*;
 public class StringOperations {
     public static void main(String[] args) {
-        String st = "Beeja";
-        String at = "Divya";
-        String a = st+" "+at;
-        System.out.println(st.length());
-        System.out.println(a);
-        char ch;
-        String rs = "";
-        for (int i=0;i<a.length();i++){
-            ch =a.charAt(i);
-            rs = ch+rs;
+        Scanner sc = new Scanner(System.in);
+        String input1 = sc.nextLine();
+        String input2 = sc.nextLine();
+        System.out.println("String Length input1: " + input1.length());
+        System.out.println("String Length input1: " + input2.length());
+        String merge = input1 + input2;
+        System.out.println("Merge of input1 and input2: " + merge);
+        System.out.println("Split of input2: " + input2.substring(2, 5));
+        if (input1.equals(input2)) {
+            System.out.println("Both are equal");
+
+        } else {
+            System.out.println("Not equal");
+
         }
-        System.out.println(rs);
-        if (st.equals(at) == true) {
-            System.out.println("Yes");
-        }
-        else {
-            System.out.println("No");
-        }
+        StringBuffer sb = new StringBuffer(input1);
+        sb.reverse();
+        System.out.println("Reverse of input1 " + sb);
     }
 }
